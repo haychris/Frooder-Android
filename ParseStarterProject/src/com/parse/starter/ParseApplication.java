e16e3f44984eaf928a1ac2efd818df29b1956d46
haychris <chay@princeton.edu>
2014-11-29 12:59:02 -0500

    Pre-release, needs global map, location, and Parse setup Also needs scraped data to be analyzed and automatically put as food items. Working: 1. Storing to Parse/ add food ability 2. Retrieving from Parse 3. Push notifications 4. Map display skeleton 5. FoodListing list and listadapter 6. Chance of success color display for food 7. Scraping from Princeton listserv successful

---
+++ ParseStarterProject/src/com/parse/starter/ParseApplication.java
@@ -0,0 +1,27 @@
+package com.parse.starter;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseUser;
+
+public class ParseApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    // Add your initialization code here
+    Parse.initialize(this, YOUR_APPLICATION_ID, YOUR_CLIENT_KEY);
+
+
+    ParseUser.enableAutomaticUser();
+    ParseACL defaultACL = new ParseACL();
+      
+    // If you would like all objects to be private by default, remove this line.
+    defaultACL.setPublicReadAccess(true);
+    
+    ParseACL.setDefaultACL(defaultACL, true);
+  }
+}
